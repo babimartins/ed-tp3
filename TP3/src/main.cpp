573dd367375d1aad2018b6db37e3c150b879e1ec
Bárbara Martins <martins.barbara04@gmail.com>
2022-07-17 14:03:47 -0300

    Update main to get input and output file names

--- TP3/src/main.cpp
+++ TP3/src/main.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
+#include <getopt.h>
+#include "../include/files.h"
+#include "../include/msgassert.h"
 
-int main() {
-    return 0;
-};
+std::string inputFile, outputFile;
+
+void parse_args(int argc, char** argv) {
+    int c;
+    extern char* optarg;
+
+    inputFile = "";
+    outputFile = "";
+
+    while ((c = getopt(argc, argv, "i:I:O:o:")) != EOF) {
+        switch (c) {
+            case 'i':
+            case 'I':
+                inputFile = optarg;
+                break;
+            case 'o':
+            case 'O':
+                outputFile = optarg;
+                break;
+            default:
+                exit(1);
+        }
+    }
+
+    erroAssert(inputFile.length() > 0, "main - É necessário fornecer o nome do arquivo de entrada");
+    erroAssert(outputFile.length() > 0, "main - É necessário fornecer o nome do arquivo de saída");
+}
+
+
+int main(int argc, char ** argv) {
+    parse_args(argc, argv);
+    handleFiles(inputFile, outputFile);
+}
